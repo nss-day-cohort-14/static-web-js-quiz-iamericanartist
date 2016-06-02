63a2a6c470ba0c6ff3982e4c9de9398e3ff77e71
Eric Denton <eric@iamericanartist.com>
2016-06-02 16:50:27 -0500

    fixed input type - text - and added parseInt

--- quiz.js
+++ quiz.js
@@ -1,24 +1,24 @@
 
-var heightInput = document.getElementById("high")
-var charInput = document.getElementById("char")
-
-
+var heightInput = document.getElementById("high");
+var charInput = document.getElementById("char");
 
 function proceedTreeGrowth(event) {
   var myTree = {
-    height: heightInput.value,  //"high" is the input id for height eventual value and "myTree is the object"
-    character: charInput.value  //"char" is the input id for character eventual value and "myTree is the object"
+    height: parseInt(heightInput.value),  //This should be a number (parseInt)"high" is the input id (initiated ABOVE) for height eventual value and "myTree is the object"
+    character: charInput.value  //"char" is the input id (initiated ABOVE) for character eventual value and "myTree is the object"
   };
+  // console.log(" >>", typeof myTree.height); //PARSEINT WORKED!!! WOOT!
+  // console.log(" >>", typeof myTree.character); // No parseInt === STRING!
 
-  if (event.keyCode === 13) {
-    if ( !myTree.height || !myTree.character) {
-      alert("Both fields must have a value"); 
+  if (event.keyCode === 13) {  //keyCode 13 is the key location for the ENTER button
+    if ( !myTree.height || !myTree.character) { // "!asdf" is basically looking for 
+      alert("Both fields must have a value!"); 
     } else {
       myTreeSeed(myTree);
     }
   } else if (event.type === "click") {
-    if ( !myTree.height || !myTree.character) {
-      alert("Both fields must have a value"); 
+    if ( !myTree.height || !myTree.character) { //The NOT operator (!) returns true for false statements and false for true statements - so here it is valuing empty as false but making it true to run the alert
+      alert("Both fields must have a value!"); 
     } else {
       myTreeSeed(myTree);
     }
@@ -27,6 +27,8 @@ function proceedTreeGrowth(event) {
 
 function myTreeSeed(myTree) {
   var newCharacter = "";
+  // console.log(" >>>>>>", myTree.height);
+  // console.log(" >>>>>>", myTree.character);
   for (var i = 0; i <= myTree.height; i++) {
     for (var j = i; j < myTree.height; j++){  
       newCharacter += " ";  
@@ -36,12 +38,10 @@ function myTreeSeed(myTree) {
     }
     newCharacter += "\n"; //I am building this all in "one" output line, so I need a "break" introduced
   }
-  console.log(newCharacter);
-
-  console.log("Our tree height is ", myTree.height);  //Doesn't really work like "it should" keeps popping up multiple times
-  console.log("Our character to use is ", myTree.character);  //Doesn't really work like "it should" keeps popping up multiple times
+  console.log(newCharacter); //THIS IS THE TREE
+  console.log("Our tree height is ", myTree.height); //logs our height input below the tree
+  console.log("Our character to use is ", myTree.character); //logs our character used input below the tree
 }
-
 
 button.addEventListener("click", proceedTreeGrowth);
 heightInput.addEventListener("keypress", proceedTreeGrowth); 
