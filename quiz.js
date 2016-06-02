2436b89b565815f6eafa4e5c385115a8c9be6055
Eric Denton <eric@iamericanartist.com>
2016-06-02 14:08:16 -0500

    fixed spacing bugs, finished all quiz requirements - COMPLETE

--- quiz.js
+++ quiz.js
@@ -1,51 +1,49 @@
-//Original input method (just to get the tree working)
-// var myTree = {
-//   height: 9,      //should eventually reflect the user input
-//   character: "#"    //should eventually reflect the user input 
-// };
 
-// Had trouble getting the "OBJECT" to play nicely with the rest of the code (add- "myTree." to the beginning of all height and character if want it back)
-// var myTree = {
-//   height: 4,
-//   character: "*"
-// };
-// var newCharacter = "";
+var heightInput = document.getElementById("high")
+var charInput = document.getElementById("char")
 
-
-var height;
-var character;
-var newCharacter = "";
 
 
 function proceedTreeGrowth(event) {
-  // if (height === "" || character === "") {
-  //   alert("Both fields must have a value"); 
-  // } else
-   if ("click") {  //above commented out to allow tree to "grow"
-    myTreeSeed(); 
-  } else if (event.keyCode === 13) { 
-    myTreeSeed();
+  var myTree = {
+    height: heightInput.value,  //"high" is the input id for height eventual value and "myTree is the object"
+    character: charInput.value  //"char" is the input id for character eventual value and "myTree is the object"
+  };
+
+  if (event.keyCode === 13) {
+    if ( !myTree.height || !myTree.character) {
+      alert("Both fields must have a value"); 
+    } else {
+      myTreeSeed(myTree);
+    }
+  } else if (event.type === "click") {
+    if ( !myTree.height || !myTree.character) {
+      alert("Both fields must have a value"); 
+    } else {
+      myTreeSeed(myTree);
+    }
   }
 }
 
-function myTreeSeed() {
-  height = document.getElementById("high").value;  //"high" is the input id for height eventual value
-    console.log("Tree height is to be ", height);  //Doesn't really work like "it should"
-  character = document.getElementById("char").value;  //"char" is the input id for character eventual value
-    console.log("Our character to use is", character);  //Doesn't really work like "it should"
-
-  for (var i = 0; i <= height; i++) {
-    for (var j = i; j < height; j++){  
+function myTreeSeed(myTree) {
+  var newCharacter = "";
+  for (var i = 0; i <= myTree.height; i++) {
+    for (var j = i; j < myTree.height; j++){  
       newCharacter += " ";  
     }
-    for (var k = 0; k < i*2-1; k++) { //makes an ODD number of Characters
-      newCharacter += character;
+    for (var k = 0; k < i*2-1; k++) { //makes an ODD number of Characters - "1"*2-1 = 2-1 = 1 // "2"*2-1 = 4-1 = 3
+      newCharacter += myTree.character; 
     }
-    newCharacter += "\n";
+    newCharacter += "\n"; //I am building this all in "one" output line, so I need a "break" introduced
   }
   console.log(newCharacter);
+
+  console.log("Our tree height is ", myTree.height);  //Doesn't really work like "it should" keeps popping up multiple times
+  console.log("Our character to use is ", myTree.character);  //Doesn't really work like "it should" keeps popping up multiple times
 }
 
 
 button.addEventListener("click", proceedTreeGrowth);
-window.addEventListener("keypress", proceedTreeGrowth); 
+heightInput.addEventListener("keypress", proceedTreeGrowth); 
+charInput.addEventListener("keypress", proceedTreeGrowth); 
+
